03823adea28876d64e1e6799646bb701fb80e859
bellasimi <gksmf6611@gmail.com>
2022-05-17 03:33:54 +0900

    Feat: 영화 상세보기 기능 추가

---
+++ src/components/Detail.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>{{ film.Title }}</div>
+  <img :src="film.Poster" />
+</template>
+
+<script>
+export default {
+  props: {
+    film: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+}
+</script>
---
+++ src/pages/DetailPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>상세페이지</h1>
+  <Detail :film="film" />
+</template>
+
+<script>
+import Detail from '~/components/Detail'
+import { API_KEY } from '~/constants'
+
+export default {
+  components: {
+    Detail,
+  },
+  data() {
+    return {
+      film: {
+        type: Object,
+        default: () => ({}),
+      },
+    }
+  },
+  created() {
+    this.fetchDetail()
+  },
+  methods: {
+    async fetchDetail() {
+      const result = await this.$fetch(
+        `?apikey=${API_KEY}&i=${this.$route.params.imdbID}&plot=full`
+      )
+      this.film = result
+    },
+  },
+}
+</script>
