479eefc94e6832e2f9f4a6101c711a3da3014721
bellasimi <gksmf6611@gmail.com>
2022-05-17 02:46:03 +0900

    Feat: 라우터 App에 등록

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import App from '~/App'
 import { createApp } from 'vue'
 import request from '~/plugins/request'
+import router from '~/plugins/routes'
 
 const app = createApp(App)
 app.use(request)
+app.use(router)
 app.mount('#app')
