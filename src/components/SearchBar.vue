9b6f275006c801296188d17f2ae548c562da9d51
bellasimi <gksmf6611@gmail.com>
2022-05-18 03:31:50 +0900

    Feat: 검색어 유효성 검사 추가

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="search-bar">
     <input v-model="keyword" :placeholder="keyword" />
-    <button @click="$router.push(`/search/${keyword}`)">검색</button>
+    <button @click="validateKeyword">검색</button>
   </div>
 </template>
 
@@ -12,7 +12,17 @@ export default {
       keyword: '검색어를 입력해 주세요',
     }
   },
+  methods: {
+    validateKeyword() {
+      const regex = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/
+      const isInvalid =
+        regex.test(this.keyword) || this.keyword.length < 2 || this.keyword.length > 15
+      if (isInvalid) {
+        alert('검색어는 한글을 제외하고 2~15자로 입력해주세요')
+      } else {
+        this.$router.push(`/search/${this.keyword}`)
+      }
+    },
+  },
 }
 </script>
-
-
