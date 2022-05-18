15df4f349b5b98d450d20c1e88699126a455ffd6
bellasimi <gksmf6611@gmail.com>
2022-05-18 20:06:00 +0900

    Design: searchList grid로 변경

--- src/components/SearchList.vue
+++ src/components/SearchList.vue
@@ -1,12 +1,13 @@
 <template>
-  <ul class="search__list">
+  <ul class="search">
     <li
       v-for="searchItem in searchList"
       :key="searchItem.imdbID"
-      class="search__item"
+      class="search__items"
       @click="$router.push(`/detail/${searchItem.imdbID}`)"
     >
-      {{ searchItem.Title }}
+      <img class="poster" :src="searchItem.Poster" alt="poster" />
+      <div class="title">{{ searchItem.Title }}</div>
     </li>
   </ul>
 </template>
@@ -19,12 +20,59 @@ export default {
       default: () => [],
     },
   },
+  methods: {},
 }
 </script>
 
 <style lang="scss" scoped>
-.search__item {
+@use 'sass:map';
+$lg: 1000px;
+$md: 800px;
+$sm: 400px;
+
+@mixin media($display) {
+  $breakpoints: (
+    lg: $lg,
+    md: $md,
+    sm: $sm,
+  );
+
+  @media all and (max-width: map.get($breakpoints,$display)) {
+    @content;
+  }
+}
+.search {
   cursor: pointer;
   list-style: none;
+  display: grid;
+  grid-auto-rows: 300px;
+
+  @include media($lg) {
+    grid-template-columns: repeat(5, 1fr);
+  }
+
+  @include media($md) {
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  @include media($sm) {
+    grid-template-rows: 1fr;
+  }
+
+  &__items {
+    width: 90%;
+    overflow: hidden;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    .poster {
+      width: 100%;
+      height: 50%;
+      object-fit: cover;
+    }
+    .title {
+    }
+  }
 }
 </style>
