9d338ef2a92b621644273f1346640b7e6f7db6c8
bellasimi <gksmf6611@gmail.com>
2022-05-18 20:05:06 +0900

    Feat: 디바운스 기능추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import App from '~/App'
 import { createApp } from 'vue'
 import router from '~/plugins/routes'
+import debounce from '~/plugins/debounce'
 
 const app = createApp(App)
 app.use(router)
+app.use(debounce)
 app.mount('#app')
