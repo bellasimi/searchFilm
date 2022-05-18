72166a60c43053015a903a14159493942b2fb7cc
bellasimi <gksmf6611@gmail.com>
2022-05-18 22:44:24 +0900

    Fix: searchDetail params 수정

--- functions/searchDetail.js
+++ functions/searchDetail.js
@@ -2,9 +2,9 @@ const axios = require('axios')
 const { API_END_POINT, API_KEY } = process.env
 
 exports.handler = async function (event) {
-  const { id } = event.queryStringParameters
+  const { i } = event.queryStringParameters
   try {
-    const { data } = await axios.get(`${API_END_POINT}?apikey=${API_KEY}&id=${id}`)
+    const { data } = await axios.get(`${API_END_POINT}?apikey=${API_KEY}&i=${i}`)
     return {
       statusCode: 200,
       body: JSON.stringify(data),
